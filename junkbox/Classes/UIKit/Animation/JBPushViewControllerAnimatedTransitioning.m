f905812f5552e3cde67984a5a5f987d917f592c5
Kenzan Hase <kenzan8000@gmail.com>
2014-02-10 19:23:18 +0900

    fix transition effect

--- junkbox/Classes/UIKit/Animation/JBPushViewControllerAnimatedTransitioning.m
+++ junkbox/Classes/UIKit/Animation/JBPushViewControllerAnimatedTransitioning.m
@@ -17,35 +17,22 @@
     UIViewController *toVC = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
 
     CGRect endFrame = [transitionContext initialFrameForViewController:fromVC];
-
-    fromVC.view.frame = endFrame;
+    [transitionContext.containerView addSubview:toVC.view];
     [transitionContext.containerView addSubview:fromVC.view];
 
-    UIView *toView = [toVC view];
-    [transitionContext.containerView addSubview:toView];
+    fromVC.view.alpha = 1.0f;
+    fromVC.view.transform = CGAffineTransformIdentity;
+    toVC.view.transform = CGAffineTransformMakeTranslation(CGRectGetWidth(endFrame), 0);
 
-    CGRect startFrame = toView.frame;
-    endFrame = startFrame;
-
-    startFrame.origin.x += CGRectGetWidth(startFrame);
-    toView.frame = startFrame;
-
-    UIView *fromView = [fromVC view];
-    CGRect outgoingEndFrame = fromView.frame;
-    outgoingEndFrame.origin.x -= CGRectGetWidth(outgoingEndFrame);
-
-    fromView.transform = CGAffineTransformMakeScale(1.0f, 1.0f);
-    fromView.alpha = 1.0f;
     [UIView animateWithDuration:[self transitionDuration:transitionContext]
                           delay:0.0f
                         options:UIViewAnimationOptionCurveEaseOut
                      animations:^ () {
-        fromView.transform = CGAffineTransformMakeScale(0.9f, 0.9f);
-        fromView.alpha = 0.5f;
-        toView.frame = endFrame;
+        fromVC.view.transform = CGAffineTransformMakeScale(0.9f, 0.9f);
+        fromVC.view.alpha = 0.5f;
+        toVC.view.transform = CGAffineTransformMakeTranslation(endFrame.origin.x, 0);
     }
-                    completion:^ (BOOL finished) {
-        [toView setNeedsUpdateConstraints];
+                     completion:^ (BOOL finished) {
         [transitionContext completeTransition:YES];
     }];
 }
