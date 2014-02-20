4d7880c1b115db49492efacb84934885718efcef
Kenzan Hase <kenzan8000@gmail.com>
2014-02-21 04:34:02 +0900

    fix JBSettingNavigationController bug

---
+++ junkbox/Classes/Setting/JBSettingNavigationController.h
@@ -0,0 +1,13 @@
+#import "JBNavigationController.h"
+
+
+#pragma mark - JBSettingNavigationController
+/// 設定
+@interface JBSettingNavigationController : JBNavigationController {
+}
+
+
+#pragma mark - property
+
+
+@end
